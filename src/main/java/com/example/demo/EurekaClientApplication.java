c310896c53944e593c3398eb7ba6f629c46187e8
Nilay Sonker <nilaysonker@gmail.com>
2021-03-28 15:19:55 +0530

    Converting jar to war

--- src/main/java/com/example/demo/EurekaClientApplication.java
+++ src/main/java/com/example/demo/EurekaClientApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @ComponentScan
 @EnableEurekaClient
 //@ComponentScan(useDefaultFilters = false)
-public class EurekaClientApplication {
+public class EurekaClientApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
